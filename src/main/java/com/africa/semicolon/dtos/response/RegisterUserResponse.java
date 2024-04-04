0e5252dcd95c4b00cd78bed3d66bb023e310b6ca
Lawal Toheeb <lawaltoheeb36@gmail.com>
2024-04-04 10:43:26 +0100

    Register user response

--- src/main/java/com/africa/semicolon/dtos/response/RegisterUserResponse.java
+++ src/main/java/com/africa/semicolon/dtos/response/RegisterUserResponse.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class RegisterUserResponse {
-    private String firstname;
-    private String lastname;
-
+    private String id;
+    private String username;
+    private String dateRegistered;
 }
